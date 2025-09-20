e314d7e82914d2fdd2ed9dd7d103959df700086b
SandipM03 <sandipmandal816708@gmail.com>
2025-09-20 23:52:42 +0530

    feat: restructure layout component for improved readability and maintainability

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "../components/header";
 import { ClerkProvider } from "@clerk/nextjs";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -11,20 +12,22 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <ClerkProvider>
-    <html lang="en">
-      <body
-        className={`${inter.className}`}>
+    <ClerkProvider
+      afterSignInUrl="/dashboard"
+      afterSignUpUrl="/dashboard"
+    >
+      <html lang="en">
+        <body className={`${inter.className}`}>
           <Header />
           <main className="min-h-screen">{children}</main>
-        
-        <footer className="bg-blue-50 py-12">
-          <div className="container mx-auto px-4 text-center text-gray-600">
-            <p>© 2024 FinanceJini. All rights reserved.</p>
-          </div>
-        </footer>
-      </body>
-    </html>
+          
+          <footer className="bg-blue-50 py-12">
+            <div className="container mx-auto px-4 text-center text-gray-600">
+              <p>© 2024 FinanceJini. All rights reserved.</p>
+            </div>
+          </footer>
+        </body>
+      </html>
     </ClerkProvider>
   );
 }
