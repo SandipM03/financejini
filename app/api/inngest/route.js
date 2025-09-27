e850c6361bb487a5580953b87ca7e77afc0c6d20
SandipM03 <sandipmandal816708@gmail.com>
2025-09-28 00:32:55 +0530

    feat: add checkBudgetAlert function for monitoring budget usage and sending alerts

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,10 +1,10 @@
 import { serve } from "inngest/next";
 import { inngest } from "@/app/lib/inngest/client";
-
+import { checkBudgetAlert } from "@/app/lib/inngest/functions";
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
      // <-- This is where you'll always add all your functions
-
+     checkBudgetAlert
   ],
 });
