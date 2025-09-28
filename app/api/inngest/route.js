5988f4de79b74cbddfddce653f5c3108f572054f
SandipM03 <sandipmandal816708@gmail.com>
2025-09-29 02:36:38 +0530

    feat: add generateMonthlyReports function and integrate AI insights for monthly financial summaries

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,12 +1,12 @@
 import { serve } from "inngest/next";
 import { inngest } from "@/lib/inngest/client";
-import { checkBudgetAlert } from "@/lib/inngest/functions";
+import { checkBudgetAlert, generateMonthlyReports } from "@/lib/inngest/functions";
 import { triggerRecurringTransactions } from "@/lib/inngest/functions";
 import { processRecurringTransaction } from "@/lib/inngest/functions";
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
      // <-- This is where you'll always add all your functions
-     checkBudgetAlert,triggerRecurringTransactions,processRecurringTransaction
+     checkBudgetAlert,triggerRecurringTransactions,processRecurringTransaction,generateMonthlyReports
   ],
 });
